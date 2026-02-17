b507be2308d966874e8e73ad93f41491c987a7af
sayeedsym <asayeed@emich.edu>
2026-02-16 22:59:00 -0500

    added updated hungry method to HUNGRY.java file

--- src/Hungry.java
+++ src/Hungry.java
@@ -5,5 +5,8 @@ public class Hungry {
 		System.out.println("We are all hungry.");
 
 	}
+	public static void hungryUpdated() {
+		System.out.println("This is a brand new line of code written by SAYEED");
+	}
 
 }
